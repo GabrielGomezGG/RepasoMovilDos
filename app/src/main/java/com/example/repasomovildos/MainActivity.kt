f7accf9e7d229e24d5a510cbcbf134e290896b93
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-02 00:03:19 -0300

    feat: implement UIState

--- app/src/main/java/com/example/repasomovildos/MainActivity.kt
+++ app/src/main/java/com/example/repasomovildos/MainActivity.kt
@@ -4,21 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.repasomovildos.ui.theme.MainViewModel
 import com.example.repasomovildos.ui.theme.RepasoMovilDosTheme
+import com.example.repasomovildos.ui.theme.ResponseUiState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -41,12 +45,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(mainViewModel: MainViewModel) {
-    val posts by mainViewModel.posts.observeAsState(emptyList())
-    
-    LazyColumn(){
-        items(posts){
-            Text(text = "Title: ${it.title}")
-            Spacer(modifier = Modifier.size(22.dp))
+    val posts by mainViewModel.posts.observeAsState(ResponseUiState.Loading)
+
+    when(posts){
+        is ResponseUiState.Failure -> TODO()
+        ResponseUiState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        is ResponseUiState.Success -> {
+            LazyColumn() {
+                items((posts as ResponseUiState.Success).posts) {
+                    Text(text = "Title: ${it.title}")
+                    Spacer(modifier = Modifier.size(22.dp))
+                }
+            }
         }
     }
+    
+
 }
--- app/src/main/java/com/example/repasomovildos/ui/theme/MainViewModel.kt
+++ app/src/main/java/com/example/repasomovildos/ui/theme/MainViewModel.kt
@@ -14,13 +14,19 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
+sealed interface ResponseUiState {
+    object Loading : ResponseUiState
+    data class Success(val posts: List<Post>) : ResponseUiState
+    data class Failure(val error: Throwable) : ResponseUiState
+}
+
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val postRepository: PostRepository
 ) : ViewModel() {
 
-    private val _posts = MutableLiveData<List<Post>>()
-    val posts : LiveData<List<Post>> = _posts
+    private val _posts = MutableLiveData<ResponseUiState>(ResponseUiState.Loading)
+    val posts : LiveData<ResponseUiState> = _posts
 
     init{
         getTodos()
@@ -30,7 +36,7 @@ class MainViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.IO) {
             val todos = postRepository.getPost()
 
-            _posts.postValue(todos)
+            _posts.postValue(ResponseUiState.Success(todos))
         }
     }
 
